7a0f14356104b05710e7f7dbfb412dc960a50a0c
Eugene <abugaeugene@gmail.com>
2024-07-06 17:17:14 +0300

    more changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,1 +1,18 @@
-// Write your Layout component here
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Slider from "../components/Slider";
+
+const Layout = ({ children }) => {
+
+    return (
+        <>
+            <Slider />
+            <Header />
+            <main>{children}</main>
+            <Outlet />
+        </>
+    );
+}
+
+export default Layout;
